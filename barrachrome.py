f536199ccb4bda3e4e5809b64851cac13593afe9
maizefraga <87202439+maizefraga@users.noreply.github.com>
2022-12-19 09:48:52 -0400

    Update barrachrome.py

--- barrachrome.py
+++ barrachrome.py
@@ -47,33 +47,46 @@ if (registros % 150 != 0):
 else:
     quantidade_paginas = registros//150
 
-# DECLARAR MAP
+# DECLARAR MAP, SERÁ UM DICIONÁRIO DE DICIONÁRIOS
 map = {}
+
+# DECLARAR NOME, SERÁ UMA LISTA COM APENAS O NOME DOS SERVIDORES
+# ISSO SERÁ ÚTIL APENAS PARA A COMPARAÇÃO COM A lista_de_nomes
 nomes = []
 
+
 # FUNÇÃO PARA LER A TABELA DENTRO DA LUPA
-
-
 def ler_tab_interna(map, nomes):
+    # O MEU 'tr' NÃO TEM CLASSE ENTÃO PRECISA SER ACESSADO DA SEGUINTE MANEIRA
     table = navegador.find_element(By.ID, 'TABLE3')
     tbody = table.find_element(By.TAG_NAME, 'tbody')
     tr = tbody.find_elements(By.TAG_NAME, 'tr')
-
+    
+    # 'td' QUE CONTÉM A MATRÍCULA
     td = tr[0].find_elements(By.TAG_NAME, 'td')
     matricula = td[2].get_property('innerText')
-
+ 
+    # 'td' QUE CONTÉM O NOME
     td = tr[2].find_elements(By.TAG_NAME, 'td')
     nome = td[2].get_property('innerText')
-
+ 
+    # 'td' QUE CONTÉM O SALÁRIO
     td = tr[6].find_elements(By.TAG_NAME, 'td')
     salario = td[2].get_property('innerText')
-
+ 
+    # 'td' QUE CONTÉM O CARGO
     td = tr[8].find_elements(By.TAG_NAME, 'td')
     cargo = td[2].get_property('innerText')
-
+ 
+    # 'td' QUE CONTÉM A LOTAÇÃO
     td = tr[12].find_elements(By.TAG_NAME, 'td')
     lotacao = td[2].get_property('innerText')
-
+ 
+    # EU PODERIA TER FEITO DA SEGUINTE MANEIRA:
+#    map.update({matricula: {'matricula': matricula, 'nome': nome,
+#                        'salario': salario, 'cargo': cargo, 'lotacao': lotacao}})
+#   nomes.append(nome)
+    # MAS PARA MELHOR COMPREENÇÃO VIZUAL DA MINHA PARTE PREFERI FAZER ASSIM:
     aux = {matricula: {'matricula': matricula, 'nome': nome,
                        'salario': salario, 'cargo': cargo, 'lotacao': lotacao}}
     map.update(aux)
@@ -81,11 +94,10 @@ def ler_tab_interna(map, nomes):
     nomes.append(aux)
     print("tabela interna lida")
 
-    return map
-
-# FUNÇÃO PARA LER A PÁGINA, ENTRAR E LER O CONTEÚDO DE TODAS AS LUPAS
+    return map, nomes
 
 
+# FUNÇÃO PARA LER A PÁGINA (ENCONTRAR TODAS AS LUPAS), ENTRAR EM CADA UMA, LER O CONTEÚDO E VOLTAR PARA A PÁGINA DE ORIGEM
 def ler_tabela(map, nomes):
     table = navegador.find_element(By.ID, 'grid')
     tbody = table.find_element(By.TAG_NAME, 'tbody')
@@ -117,6 +129,7 @@ def ler_tabela(map, nomes):
 
 
 sleep(3)
+
 # O RÔLE BASICAMENTE
 count = 1
 j = 0
@@ -134,19 +147,24 @@ print(map)
 
 navegador.close()
 
+# CRIEI UM ARQUIVO JSON COM TODOS OS DADOS RASPADOS
 file = open('map.json', 'w')
 json.dump(map, file, ensure_ascii=False)
 file.close()
 
+# DECLAREI UMA LISTA DE NOMES ALEATÓRIOS COM ALGUNS NOMES DE SERVIDORES
 lista_de_nomes = ['ABADIA BONFIM MARTINS GALLE', 'MAÍZE FRAGA SOUZA',
                   'ALKFJAIGJGJAREJ JAIWEJAO', 'AOIJOAIHGIO', 'LUENE PEREIRA DE SOUZA']
 
+# FIZ A INTERSECÇÃO DESSES NOMES COM OS NOMES DOS SERVIDORES
 nomes_inersecao = list(set(nomes).intersection(set(lista_de_nomes)))
 
+# EXTRAÍ DO DICIONÁRIO DE DICIONÁRIOS (O RESULTADO TOTAL DA RASPAGEM) APENAS OS DADOS DOS NOMES RESULTANTES DA INTERSECÇÃO
 dict_intersecao = {servidor[0]: servidor[1] for servidor in map.items(
 ) if servidor[1]['nome'] in nomes_inersecao}
 print(dict_intersecao)
 
+# CRIEI UM ARQUIVO JSON COM TODOS OS DADOS DA INTERSECÇÃO
 file = open('intersecao.json', 'w')
 json.dump(dict_intersecao, file, ensure_ascii=False)
 file.close()
