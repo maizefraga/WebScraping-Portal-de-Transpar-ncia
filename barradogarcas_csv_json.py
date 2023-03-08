3b035412d1f807e1a5826c152e999e992f96638d
maizefraga <87202439+maizefraga@users.noreply.github.com>
2023-03-08 10:49:11 -0400

    Add files via upload

---
+++ barradogarcas_csv_json.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+
+servidores = f"/home/maizefs/Downloads/projeto_de_pesquisa/scraping_tranparencia/map.json"
+
+
+df_servidores = pd.read_json(servidores)
+
+# Salvar os dados dos servidores em um arquivo CSV
+df_servidores.to_json("map.json")
+
+
+# Carregar os dados dos alunos dos anos 2020 a 2023
+urls_alunos = [
+    "https://dados.ifmt.edu.br/dataset/6b7c7c38-587a-436b-a7b2-4e3ca59d1ca8/resource/fc30244a-166d-4830-a66d-a573ebe187eb/download/aluno.csv",
+    "https://dados.ifmt.edu.br/dataset/6b7c7c38-587a-436b-a7b2-4e3ca59d1ca8/resource/d2a472aa-3a02-4d45-bc65-5852fa9be664/download/aluno.csv",
+    "https://dados.ifmt.edu.br/dataset/6b7c7c38-587a-436b-a7b2-4e3ca59d1ca8/resource/96f114e2-58f9-4f59-9c44-f59814c0b264/download/aluno.csv",
+    "https://dados.ifmt.edu.br/dataset/6b7c7c38-587a-436b-a7b2-4e3ca59d1ca8/resource/1535c8e2-f1c4-4dcf-9948-dc734522d040/download/aluno.csv",
+]
+# cria um lista vazia
+dfs_alunos = []
+
+
+# percorre os links e adiociona na lista anterio
+for url in urls_alunos:
+    df = pd.read_csv(url, encoding="utf-8", sep=",")
+    dfs_alunos.append(df)
+
+
+# Concatenar os dataframes dos alunos em um único dataframe
+df_alunos = pd.concat(dfs_alunos)
+df_alunos['nome'] = df_alunos['nome'].str.upper()
+# __import__('ipdb').set_trace()
+# Juntar os dataframes dos servidores e dos alunos com base no nome
+df_merge = pd.merge(df_servidores.T, df_alunos, how="inner",
+                    left_on="nome", right_on="nome")
+
+# apaga os nomes duplicados na lista
+df_merge = df_merge.drop_duplicates(subset='nome', keep='first')
+
+
+print("\n**********************************************************************************************************************************************************************************************************")
+print("\n Esses são os alunos que estudaram no IF e agora trabalham para o governo")
+print("\n********************************************************************************************************************************************************************************************************\n \n")
+
+
+print(df_merge)
